d064601ff5863db672e72a02d3916c205bc80d7d
m1l0js <m1l0js@proton.me>
2024-05-16 01:09:51 +0200

    Update autorecon_installation.sh

--- autorecon_installation.sh
+++ autorecon_installation.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 sudo apt update -y
-sudo apt install python3 -y
-sudo apt install python3-pip -y
-sudo apt install seclists
-sudo apt install seclists curl dnsrecon enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y
+sudo apt install python3 python3-pip seclists pipx  curl dnsrecon enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y
 sudo apt install python3-venv -y
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
